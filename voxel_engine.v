2873f689be54d02b3741fe590682bb60f8e15075
experimentech <experimentech@gmail.com>
2024-11-29 13:46:43 +1100

    more fixes.

--- voxel_engine.v
+++ voxel_engine.v
@@ -24,22 +24,38 @@ module voxel_engine(
     end
   end
 
+  integer x, y, z;
+
   // Simple projection algorithm
   always @(posedge clk or posedge reset) begin
     if (reset) begin
       addr <= 0;
       we <= 0;
+      x <= 0;
+      y <= 0;
+      z <= 0;
     end else if (display_on) begin
-      integer x, y, z;
-      for (x = 0; x < 8; x = x + 1) begin
-        for (y = 0; x < 8; y = y + 1) begin
-          for (z = 0; z < 8; z = z + 1) begin
-            if (voxels[x][y][z]) begin
-              // Simple orthographic projection
-              addr <= {y[3:0], x[3:0], 4'b0}; // Ensure addr is 12 bits
-              we <= 1;
-              ram_d <= 8'b11111111; // White voxel
-            end
+      if (voxels[x][y][z]) begin
+        // Simple orthographic projection
+        addr <= {y[3:0], x[3:0], 4'b0}; // Ensure addr is 12 bits
+        we <= 1;
+        ram_d <= 8'b11111111; // White voxel
+      end else begin
+        we <= 0;
+      end
+      // Increment indices
+      if (z < 7) begin
+        z <= z + 1;
+      end else begin
+        z <= 0;
+        if (y < 7) begin
+          y <= y + 1;
+        end else begin
+          y <= 0;
+          if (x < 7) begin
+            x <= 0 + 1;
+          end else begin
+            x <= 0;
           end
         end
       end
